f24dcf046e7f0a2b65bb774bb530411936023488
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-04-09 13:33:25 +0200

    update boruta + controller generation

---
+++ lib/boruta_example_web/controllers/openid/userinfo_controller.ex
@@ -0,0 +1,30 @@
+defmodule BorutaExampleWeb.Openid.UserinfoController do
+  @behaviour Boruta.Openid.UserinfoApplication
+
+  use BorutaExampleWeb, :controller
+
+  alias BorutaExampleWeb.OpenidView
+
+  def openid_module, do: Application.get_env(:boruta_example, :openid_module, Boruta.Openid)
+
+  def userinfo(conn, _params) do
+    openid_module().userinfo(conn, __MODULE__)
+  end
+
+  @impl Boruta.Openid.UserinfoApplication
+  def userinfo_fetched(conn, userinfo) do
+    conn
+    |> put_view(OpenidView)
+    |> render("userinfo.json", userinfo: userinfo)
+  end
+
+  @impl Boruta.Openid.UserinfoApplication
+  def unauthorized(conn, error) do
+    conn
+    |> put_resp_header(
+      "www-authenticate",
+      "error=\"#{error.error}\", error_description=\"#{error.error_description}\""
+    )
+    |> send_resp(:unauthorized, "")
+  end
+end
--- lib/boruta_example_web/views/openid_view.ex
+++ lib/boruta_example_web/views/openid_view.ex
@@ -4,4 +4,8 @@ defmodule BorutaExampleWeb.OpenidView do
   def render("jwks.json", %{jwk_keys: jwk_keys}) do
     %{keys: jwk_keys}
   end
+
+  def render("userinfo.json", %{userinfo: userinfo}) do
+    userinfo
+  end
 end
