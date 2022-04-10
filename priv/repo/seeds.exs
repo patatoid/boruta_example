3ac7e9492ab450aea0956f83e9a27d3db3cdd4e8
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-04-10 16:52:22 +0200

    add a second oauth client seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -44,3 +44,12 @@ Boruta.Ecto.Admin.create_client(
     ]
   }
 )
+Boruta.Ecto.Admin.create_client(
+  %{
+    id: System.get_env("SECOND_OAUTH_CLIENT_ID"),
+    secret: System.get_env("SECOND_OAUTH_CLIENT_SECRET"),
+    redirect_uris: [
+      "https://www.certification.openid.net/test/a/boruta_example/callback"
+    ]
+  }
+)
